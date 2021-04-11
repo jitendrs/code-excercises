46371996493bd09cd2f852e715508dc00c5b51dd
jitendrs <jitendra.sapariya@gmail.com>
2021-04-11 11:51:23 +0530

    First Go Program

---
+++ go/HelloWorld.go
@@ -0,0 +1,13 @@
+/*
+First go program from The go programming language page:1
+Program: Hello World
+Description: To print hello world on console
+
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World From Go..")
+}
