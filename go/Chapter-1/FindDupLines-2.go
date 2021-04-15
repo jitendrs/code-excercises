d63bf61e79b92250560e2e9d2e0f86cc9af60db9
jitendrs <jitendra.sapariya@gmail.com>
2021-04-15 09:19:11 +0530

    Animated GIF image example

--- go/Chapter-1/FindDupLines-2.go
+++ go/Chapter-1/FindDupLines-2.go
@@ -1,41 +1,31 @@
 /*
-Description: Go program to prints and count the text of lines that appear more than once in the input.
+Description: Go program to prints and count the text of lines that appear more than once in the input using io/ioutil.
 			 It reads from stdin or from a list of named files from The go programming language book page:10
 */
 
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
 	counts := make(map[string]int)
-	files := os.Args[1:]
-	if len(files) == 0 {
-		countLines(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
-			}
-			countLines(f, counts)
-			f.Close()
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range string.Split(string(data), "\n") {
+			counts[line]++
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Print("%d\t%s\n", n, line)
 		}
-	}
-}
-
-func countLines(f *os.File, counts map[string]int) {
-	input := bufio.NewScanner(f)
-	for input.Scan() {
-		counts[input.Text()]++
 	}
 }
