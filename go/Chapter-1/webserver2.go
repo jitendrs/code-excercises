53920afb6938e60a5cd5998443b9145d5221c827
jitendrs <jitendra.sapariya@gmail.com>
2021-04-20 17:07:49 +0530

    Web Server examples

---
+++ go/Chapter-1/webserver2.go
@@ -0,0 +1,80 @@
+/*
+Web Server with minimal "echo" and counter server.
+*/
+
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0 // first colour in palette
+	blackIndex = 1 // next colour in palette
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	http.HandleFunc("/lissa", lissa)
+	log.Fatal(http.ListenAndServe("localhost:8282", nil))
+}
+
+// handler echoes the Path component of the requested URL.
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+// counter echoes the number of calls so far.
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
+
+func lissa(w http.ResponseWriter, r *http.Request) {
+	lissajous(w)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complex x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
