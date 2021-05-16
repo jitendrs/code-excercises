2c5325f5c1160e362fea71fc7968f7be8e03bb9e
Jitendra Sapariya <Jitendra.Sapariya@team.telstra.com>
2021-05-16 12:27:14 +0530

    New Function Index to get index of substring

--- go/Chapter-3/stringFunction/stringFunctions.go
+++ go/Chapter-3/stringFunction/stringFunctions.go
@@ -6,28 +6,26 @@ Description: code to check if string has prefix/suffix or provided substrings.
 
 package main
 
-import (
-	"fmt"
-	"os"
-)
+import "fmt"
 
 func main() {
-	if HasPrefix(os.Args[1], os.Args[2]) {
-		fmt.Printf("Prifix %s is in the string: %s\n", os.Args[1], os.Args[2])
-	} else {
-		fmt.Printf("Prifix %s is not the string: %s\n", os.Args[1], os.Args[2])
-	}
+	// if HasPrefix(os.Args[1], os.Args[2]) {
+	// 	fmt.Printf("Prifix %s is in the string: %s\n", os.Args[1], os.Args[2])
+	// } else {
+	// 	fmt.Printf("Prifix %s is not the string: %s\n", os.Args[1], os.Args[2])
+	// }
 
-	if HasSuffix(os.Args[1], os.Args[2]) {
-		fmt.Printf("Sufix %s is in the string: %s\n", os.Args[2], os.Args[1])
-	} else {
-		fmt.Printf("Sufix %s is not the string: %s\n", os.Args[2], os.Args[1])
-	}
-	if Contains(os.Args[1], os.Args[2]) {
-		fmt.Printf("string  %s is substring of : %s\n", os.Args[2], os.Args[1])
-	} else {
-		fmt.Printf("string  %s is not substring: %s\n", os.Args[2], os.Args[1])
-	}
+	// if HasSuffix(os.Args[1], os.Args[2]) {
+	// 	fmt.Printf("Sufix %s is in the string: %s\n", os.Args[2], os.Args[1])
+	// } else {
+	// 	fmt.Printf("Sufix %s is not the string: %s\n", os.Args[2], os.Args[1])
+	// }
+	// if Contains(os.Args[1], os.Args[2]) {
+	// 	fmt.Printf("string  %s is substring of : %s\n", os.Args[2], os.Args[1])
+	// } else {
+	// 	fmt.Printf("string  %s is not substring: %s\n", os.Args[2], os.Args[1])
+	// }
+	fmt.Println(Index("adwdcddabcwew", "abc"))
 }
 
 func HasPrefix(s, prefix string) bool {
@@ -45,4 +43,13 @@ func Contains(s, substr string) bool {
 		}
 	}
 	return false
+}
+
+func Index(s, sep string) int {
+	for i := 0; i < len(s); i++ {
+		if HasPrefix(s[i:], sep) {
+			return i
+		}
+	}
+	return -1
 }
