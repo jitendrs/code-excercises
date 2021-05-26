634a84405afb284a30f355a9312dccce316f6d84
jitendrs <Jitendra.Sapariya@gmail.com>
2021-05-26 17:22:12 +0530

    Slices exaple

---
+++ go/Chapter-4/SlicesExamples.go
@@ -0,0 +1,15 @@
+/*
+Go program to modification of orignal array elements from The go programming language book page:84
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	months := [...]string{1: "January",2:"" 12: "December"}
+	fmt.Println(months)
+}
